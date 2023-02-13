96e227827a2e63302206766ffb86ca838d3f1311
Wade Urry <wade@iwader.co.uk>
2023-02-13 20:25:41 +0000

    Add gh headers

--- src/data/github.js
+++ src/data/github.js
@@ -9,6 +9,8 @@ module.exports = async function () {
     fetchOptions: {
       headers: {
         Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+        Accept: 'application/vnd.github+json',
+        'X-GitHub-Api-Version': '2022-11-28',
       },
     },
   }
