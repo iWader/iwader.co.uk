4a36148575068e34664c4d8dc3ef2e554104dc0a
Wade Urry <wade@iwader.co.uk>
2024-01-21 17:00:17 +0000

    Add map image where available

--- workers/strava/src/activities.ts
+++ workers/strava/src/activities.ts
@@ -26,7 +26,20 @@ export default {
 			return new Response(JSON.stringify(body), { status: 200 })
 		}
 
-		body.activities = await getStravaAthleteActivities(accessToken)
+		body.activities = (await getStravaAthleteActivities(accessToken)).slice(0, 6)
+
+		const promises = body.activities.map(async (activity: StravaActivity) => {
+			if (activity.map && activity.map.summary_polyline) {
+				const overlay = `path-2+9856E8-1+9856E8-1(${encodeURIComponent(activity.map.summary_polyline)})`
+				const url = new URL(`https://api.mapbox.com/styles/v1/${env.MAPBOX_USERNAME}/${env.MAPBOX_STYLE_ID}/static/${overlay}/auto/640x640@2x?access_token=${env.MAPBOX_ACCESS_TOKEN}`)
+
+				activity.map.image = url.toString()
+			}
+
+			return activity
+		})
+
+		body.activities = await Promise.all(promises)
 
 		return new Response(JSON.stringify(body), { status: 200 })
 	},
--- workers/strava/worker-configuration.d.ts
+++ workers/strava/worker-configuration.d.ts
@@ -3,4 +3,7 @@ interface Env {
 	OAUTH_STATE: string
 	STRAVA_CLIENT_ID: string
 	STRAVA_CLIENT_SECRET: string
+	MAPBOX_ACCESS_TOKEN: string
+	MAPBOX_USERNAME: string
+	MAPBOX_STYLE_ID: string
 }
